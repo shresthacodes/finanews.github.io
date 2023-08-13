c6b9fa47a23357cae0551b2ee841ba8dbf490ab0
Shrestha <138806766+shresthacodes@users.noreply.github.com>
2023-08-13 21:46:02 +0530

    Update main.js

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ function bindData(articles) {
 
     cardsContainer.innerHTML = "";
 
-    articles.forEach((article) => {
+    articles.map((article) => {
         if (!article.urlToImage) return;
         const cardClone = newsCardTemplate.content.cloneNode(true);
         fillDataInCard(cardClone, article);
